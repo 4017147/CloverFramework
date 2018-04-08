71e1efb2c11e6151f71342bfa21a68259f75c481
LoongYou <815234949@qq.com>
2018-04-09 05:56:34 +0800

    recommit

---
+++ src/com/dict/entity/User_d.java
@@ -0,0 +1,25 @@
+package com.dict.entity;
+
+
+/**
+ * @author entity-generator
+ * 
+ */
+
+public enum User_d{
+    
+	User
+    ,id
+    ,username
+    ,nick
+    ,password
+    ,email
+    ,phone
+    ,question
+    ,answer
+    ,login
+    ,logout
+    ,visit
+    ,ip
+    ,type
+}
